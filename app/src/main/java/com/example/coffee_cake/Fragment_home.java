47b3f71b4b2f8e555ca49b2dbdcb7ac7a7a76320
Khánh Đỗ Phạm Huy <20521442@ms.uit.edu.vn>
2022-05-02 15:24:41 +0700

    change bill

--- app/src/main/java/com/example/coffee_cake/Fragment_home.java
+++ app/src/main/java/com/example/coffee_cake/Fragment_home.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,12 +57,14 @@ public class Fragment_home extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    ListView listDrinks;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        listDrinks = (ListView) view.findViewById(R.id.lvDrinkStack);
 
 
 

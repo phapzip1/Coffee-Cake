0ae960f0cf18a93ae7c2a1b2ff8dce0babb2fe54
Khánh Đỗ Phạm Huy <20521442@ms.uit.edu.vn>
2022-05-03 11:02:28 +0700

    add table

--- app/src/main/java/com/example/coffee_cake/TableAdapter.java
+++ app/src/main/java/com/example/coffee_cake/TableAdapter.java
@@ -39,9 +39,8 @@ public class TableAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < getCount(); j++){
             if(table.get(i)){
-
                 view = LayoutInflater.from(context).inflate(R.layout.layout_drinks_table, viewGroup, false);
                 ((TextView)view.findViewById(R.id.title1)).setText("Bàn " + (i+1));
             }

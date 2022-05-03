91bc1c967d1928801b3a595fbc2ec64814e6eb97
skrttttttt <phaptoan333@gmail.com>
2022-05-03 18:57:50 +0700

    them ham chua cau lenh query

--- app/src/main/java/com/example/coffee_cake/DBHelper.java
+++ app/src/main/java/com/example/coffee_cake/DBHelper.java
@@ -10,6 +10,21 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private static String DBNAME = "Coffeedata.db";
     private static int DBVERSION = 1;
+    public static String MONTHLY = "SELECT SELECT strftime('%m', NGHD) AS THANG, SUM(TRIGIA) FROM HOADON ";
+
+    // lay du lieu thong ke theo thang
+    public String getMonthlyStatistic(int year)
+    {
+        return "SELECT strftime('%m', NGHD) AS THANG, SUM(TRIGIA) " +
+                "FROM HOADON WHERE NGHD LIKE '" + year + "-%' "+
+                "GROUP BY strftime('%m', NGHD) "+
+                "ORDER BY strftime('%m', NGHD) ASC";
+    }
+
+    public String getSeasonlyStatistic(int year)
+    {
+        return "";
+    }
 
     public DBHelper(@Nullable Context context) {
         super(context, DBNAME, null, DBVERSION);

179e20df31e4c01a70e6a7cc4b29caa79c08ca2b
skrttttttt <phaptoan333@gmail.com>
2022-05-16 11:02:27 +0700

    firestore

--- app/src/main/java/com/example/coffee_cake/Fragment_menu_coffee_notable.java
+++ app/src/main/java/com/example/coffee_cake/Fragment_menu_coffee_notable.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -16,6 +17,12 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -86,40 +93,54 @@ public class Fragment_menu_coffee_notable extends Fragment {
         edtcoffeeno = (EditText) v.findViewById(R.id.edtcoffeeno);
         lvcoffeeno = (ListView) v.findViewById(R.id.lvcoffeeno);
 
-        arrayList = new ArrayList<>();
-        adapter = new ProductAdapterUpdate(getActivity(),R.layout.layout_menu_drinks_notable_update,arrayList);
-        lvcoffeeno.setAdapter(adapter);
-        arrayList.clear();
+
 
 
         Bundle bundle = getArguments(); // có cái temp: tức là chọn vào cái nào của menu và số bàn
         tam = bundle.getString("temp");
+        String query = "";
 
-        Cursor cursor = null;
+        
 
         switch (tam)
         {
             case "coffee":
-                cursor = db.getReadableDatabase().rawQuery("SELECT * FROM SANPHAM WHERE MASP LIKE 'CA%' ",null);
+                query = "/SANPHAM/CAPHE/DANHSACHCAPHE";
                 break;
             case "trasua":
-                cursor = db.getReadableDatabase().rawQuery("SELECT * FROM SANPHAM WHERE MASP LIKE 'TS%' ",null);
+                query = "/SANPHAM/TRASUA/DANHSACHTRASUA";
                 break;
             case "sinhto":
-                cursor = db.getReadableDatabase().rawQuery("SELECT * FROM SANPHAM WHERE MASP LIKE 'ST%' ",null);
+                query = "/SANPHAM/SINHTO/DANHSACHSINHTO";
                 break;
             case "topping":
-                cursor = db.getReadableDatabase().rawQuery("SELECT * FROM SANPHAM WHERE MASP LIKE 'TO%' ",null);
+                query = "/SANPHAM/TOPPING/DANHSACHTOPPING";
                 break;
         }
-        while(cursor.moveToNext())
-        {
-            String MASP = cursor.getString(cursor.getColumnIndex("MASP"));
-            String TENSP = cursor.getString(cursor.getColumnIndex("TENSP"));
-            int GIA = cursor.getInt(cursor.getColumnIndex("GIA"));
-            Product temp = new Product(MASP,TENSP,GIA);
-            arrayList.add(temp);
-        }
+
+        FirebaseFirestore.getInstance().collection(query).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    arrayList = new ArrayList<>();
+                    adapter = new ProductAdapterUpdate(getActivity(),R.layout.layout_menu_drinks_notable_update,arrayList);
+                    lvcoffeeno.setAdapter(adapter);
+
+                    for (QueryDocumentSnapshot data : task.getResult())
+                    {
+                        String MASP = data.getId(),
+                                TENSP = data.getString("TEN");
+                        Long GIA = (Long)data.get("GIA");
+
+                        arrayList.add(new Product(MASP, TENSP, GIA.intValue()));
+                    }
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+        });
+
         edtcoffeeno.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -148,7 +169,7 @@ public class Fragment_menu_coffee_notable extends Fragment {
             public void afterTextChanged(Editable editable) {
             }
         });
-        adapter.notifyDataSetChanged();
+
 
         //hue
         lvcoffeeno.setOnItemClickListener((adapterView, view, i, l) -> {
@@ -163,8 +184,6 @@ public class Fragment_menu_coffee_notable extends Fragment {
         ((ImageView)v.findViewById(R.id.btnAddDrink)).setOnClickListener(view -> {
             Navigation.findNavController(view).navigate(R.id.action_fragment_menu_coffee_notable2_to_fragment_drinks_edit);
         });
-
-
 
         return v;
     }

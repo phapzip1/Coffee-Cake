38735be5a16115b30f5db251535abf482b7cb7e8
skrttttttt <phaptoan333@gmail.com>
2022-05-25 12:18:15 +0700

    fuck

--- app/src/main/java/com/example/coffee_cake/Fragment_statistic.java
+++ app/src/main/java/com/example/coffee_cake/Fragment_statistic.java
@@ -4,8 +4,10 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,9 +23,20 @@ import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Array;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -75,8 +88,10 @@ public class Fragment_statistic extends Fragment {
         }
     }
 
-    private String[] MOY = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+    final Calendar instance = Calendar.getInstance();
 
+    private String[] MOY = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+    ArrayList<BarEntry> barEntries;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -94,24 +109,50 @@ public class Fragment_statistic extends Fragment {
         barChart.getAxisLeft().setDrawGridLines(false);
         barChart.getXAxis().setDrawGridLines(false);
         barChart.getAxisRight().setEnabled(false);
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
 
+//        BarDataSet barDataSet = new BarDataSet(barEntries, "None");
 
+//        barDataSet.setColors(Color.argb( 200,22, 208, 255)); // ocean blue
+//        barDataSet.setDrawValues(true);
+//        barChart.setData(new BarData(barDataSet));
+//        barChart.animateY(3000);
 
-
-
-
-
-        BarDataSet barDataSet = new BarDataSet(barEntries, "None");
-        barDataSet.setColors(Color.argb( 200,22, 208, 255)); // ocean blue
-        barDataSet.setDrawValues(true);
-
-        //barChart.invalidate();
-        barChart.setData(new BarData(barDataSet));
-        barChart.animateY(3000);
-
+        SetBarchart(2021);
 
         return mview;
+    }
+
+    private void SetBarchart(int Year)
+    {
+        instance.set(Year, 1, 1, 0, 0, 0);
+        long start = instance.getTimeInMillis() / 1000;
+        instance.set(Year + 1, 1, 1, 0, 0, 0);
+        long end = instance.getTimeInMillis() / 1000;
+
+        FirebaseFirestore.getInstance().collection("HOADON").whereGreaterThanOrEqualTo( "NGHD", start).whereLessThanOrEqualTo("NGHD", end).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                barEntries = new ArrayList<>();
+                for (int i = 0; i < 12; i++)
+                    barEntries.add(new BarEntry(i + 1, 0));
+
+                for (DocumentSnapshot data : task.getResult())
+                {
+                   instance.setTimeInMillis(data.getLong("NGHD") * 1000);
+                   int index = instance.get(Calendar.MONTH) - 1;
+                   barEntries.get(index).setY( barEntries.get(index).getY() + data.getLong("TRIGIA"));
+                }
+
+                for (BarEntry entry : barEntries)
+                {
+                    Log.i (entry.getX() + "", entry.getY() + "");
+                }
+
+                BarDataSet dataSet = new BarDataSet(barEntries, "");
+                barChart.setData(new BarData(dataSet));
+                barChart.animateY(3000);
+            }
+        });
     }
 
     class Formatter extends ValueFormatter
